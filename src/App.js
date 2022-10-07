5552e1a55cd6278c43c808fc9b6015309729e8d7
KateHol <Golotakaterina@gmail.com>
2022-10-07 21:11:24 +0300

    add GitHub link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Weather App
+    <div className="App">
+      <h1>Weather App</h1>
+   <footer>This project was coded by Kate Holota and is{" "}
+    <a href="https://github.com/KateHol/weather" 
+    target="_blank"
+    rel='noreferrer'>
+       open-sourced on GitHub
+      </a>
+    </footer> 
     </div>
   );
 }
