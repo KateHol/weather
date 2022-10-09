272ab29709f43e0a9d5f8b5e5f99451bdcb8ca59
KateHol <Golotakaterina@gmail.com>
2022-10-09 22:31:58 +0300

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
+import Weather from './Weather';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       <h1>Weather App</h1>
+      <Weather />
+
    <footer>This project was coded by Kate Holota and is{" "}
     <a href="https://github.com/KateHol/weather" 
     target="_blank"
@@ -13,6 +18,7 @@ function App() {
       </a> and
       <a href="https://helpful-caramel-ab529a.netlify.app"> hosted on Netlify</a>
     </footer> 
+    </div>
     </div>
   );
 }
